18289fc69833ca643d530c7feae0e88cbe40969f
Shodmon <mamayusupovshodmon2003@gmail.com>
2022-10-23 02:07:09 +0500

    third attempt to correct two mistakes

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -3,8 +3,8 @@ package lang.print.gaps.finalModuleTask;
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
         int first=number%10;
-        int second=number%100-first;
-        int third=number%1000-second-first;
+        int second=(number%100-first)/10;
+        int third=(number%1000-10*second-first)/100;
         int fourth=number/1000;
         System.out.println(first+second+third+fourth);
     }
--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,7 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
-        float fahrenheit=(float)(9*temperatureCelsius/5)+32;
+        float fahrenheit=(float)((9*temperatureCelsius/5)+32.0);
         System.out.println(fahrenheit);
     }
 }
