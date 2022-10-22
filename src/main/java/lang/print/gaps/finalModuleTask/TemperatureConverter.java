c8dc0e85d35de04fa9c4b7cbc4faa8ef9cf2c7b5
Shodmon <mamayusupovshodmon2003@gmail.com>
2022-10-23 02:18:05 +0500

    one mistake

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,7 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
-        float fahrenheit=(float)((9*temperatureCelsius/5)+32.0);
+        float fahrenheit=(float)(9*temperatureCelsius/5f+32.0);
         System.out.println(fahrenheit);
     }
 }
